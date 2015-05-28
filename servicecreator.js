b375750c690a765075d693d357b54d1db4c8aa84
Andrija Petrovic <andrija.hers@gmail.com>
2015-05-28 12:56:32 +0200

    This service remains only for test purposes

--- servicecreator.js
+++ servicecreator.js
@@ -1,3 +1,5 @@
+var pm = require('process-monitor');
+
 function createProcessStatsService(execlib,ParentServicePack){
   var execSuite = execlib.execSuite,
       taskRegistry = execSuite.taskRegistry;
@@ -15,8 +17,9 @@ function createProcessStatsService(execlib,ParentServicePack){
     ParentService.call(this,prophash);
     //execlib.execSuite.acquireSink('Time',{role:'user'}).done(this.onTimeSink.bind(this));
     taskRegistry.run('findSink',{
-      name: 'Time',
-      cb: this.onTimeSink.bind(this)
+      sinkname: 'Time',
+      identity: {role:'user',name:'user'},
+      onSink: this.onTimeSink.bind(this)
     });
   }
   ParentService.inherit(ProcessStatsService,factoryCreator);
@@ -24,7 +27,27 @@ function createProcessStatsService(execlib,ParentServicePack){
     ParentService.prototype.__cleanUp.call(this);
   };
   ProcessStatsService.prototype.onTimeSink = function(timesink){
-    //dodado
+    if(!timesink){
+      return;
+    }
+    taskRegistry.run('readState',{
+      state:taskRegistry.run('materializeState',{
+        sink:timesink
+      }),
+      name:'time',
+      cb:this.onTime.bind(this)
+    });
+  };
+  ProcessStatsService.prototype.onTime = function(time){
+    var m = pm.monitor({ pid: process.pid });
+    m.on('stats',this.onStats.bind(this,m));
+    m.start();
+  };
+  ProcessStatsService.prototype.onStats = function(m,stats){
+    m.stop();
+    m = null;
+    this.state.set('cpu',stats.cpu);
+    this.state.set('ram',~~(stats.mem/1000));
   };
   
   return ProcessStatsService;
